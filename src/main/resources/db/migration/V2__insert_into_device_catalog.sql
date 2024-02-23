185a52ccff5064f5164c7cb37356d7eda2415053
ShumkoRaman <shumkoraman@gmail.com>
2024-02-23 13:18:47 +0300

    [DATABASE] : create configure connection, create flyway

---
+++ src/main/resources/db/migration/V2__insert_into_device_catalog.sql
@@ -0,0 +1,7 @@
+INSERT INTO device_catalog ( brand, model, quantity, price, created_date, updated_date)
+VALUES
+    ('iPhone', '12', 100, 45000, '2023-04-27', '2023-09-23'),
+    ('Samsung', 'A43', 340, 27000, '2022-04-27', '2023-01-23'),
+    ('Xiaomi', 'Redmi 12', 510, 19000, '2023-02-12', '2024-01-05'),
+    ('iPhone', '15 pro', 50, 90000, '2024-02-20', '2024-02-23'),
+    ('iPhone', '15', 150, 60000, '2024-01-27', '2024-02-02')
